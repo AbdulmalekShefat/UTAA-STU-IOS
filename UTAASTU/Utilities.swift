aeb6a5ed54aeea0da9b97e50d037e55e136c3d14
Abdulmalek S. A. Shefat <abdulmalek.s.a.shefat@Abdulmaleks-iMac.local>
2017-07-28 00:13:36 +0300

    First Commit

--- UTAASTU/Utilities.swift
+++ UTAASTU/Utilities.swift
@@ -56,3 +56,53 @@ func imageFromData(imageData: String, withBlock: (_ image: UIImage?) -> Void){
     
 }
 
+extension UIImage {
+    
+    var isPortrait:     Bool    {return size.height > size.width}
+    var isLandscape:     Bool    {return size.width > size.height}
+    var breadth:        CGFloat {return min(size.width, size.height)}
+    var breadthSize:    CGSize  {return CGSize(width: breadth, height: breadth)}
+    var breadthRect:    CGRect  {return CGRect(origin: .zero, size: breadthSize)}
+    var circleMask:     UIImage? {
+        
+        UIGraphicsBeginImageContextWithOptions(breadthSize, false, scale)
+        defer { UIGraphicsEndImageContext() }
+        guard let cgImage = cgImage?.cropping(to: CGRect(origin: CGPoint(x: isLandscape ? floor((size.width - size.height)/2) : 0, y: isPortrait ? floor((size.height - size.width)/2) : 0), size: breadthSize)) else { return nil }
+        UIBezierPath(ovalIn: breadthRect).addClip()
+        UIImage(cgImage: cgImage).draw(in: breadthRect)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    
+    }
+    
+    func scaleImagetoSize(newSize: CGSize) -> UIImage {
+        
+        var scaledImageRect = CGRect.zero
+        
+        let aspectWidth = newSize.width / size.width
+        let aspectHeight = newSize.height / size.height
+        
+        let aspectRatio = max(aspectWidth, aspectHeight)
+        
+        scaledImageRect.size.width = size.width * aspectRatio
+        scaledImageRect.size.height = size.height * aspectRatio
+        scaledImageRect.origin.x = (newSize.width - scaledImageRect.size.width) / 2.0
+        scaledImageRect.origin.y = (newSize.height - scaledImageRect.size.height) / 2.0
+        
+        UIGraphicsBeginImageContext(newSize)
+        draw(in: scaledImageRect)
+        let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return scaledImage!
+        
+    }
+    
+}
+
+
+
+
+
+
+
+
